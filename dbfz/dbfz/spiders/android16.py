be0547d9ce4230fbe4e7990df4b425f3593e509a
Diego Ramos <diego.onethreezero@gmail.com>
2018-11-23 15:04:37 -0800

    json is corrected now, should be able to implement this on every other page. wish i knew how to reformat this so it didnt take the first row of every wikitable.

--- dbfz/dbfz/spiders/android16.py
+++ dbfz/dbfz/spiders/android16.py
@@ -7,26 +7,24 @@ class Android16Spider(scrapy.Spider):
     start_urls = ['http://www.dustloop.com/wiki/index.php?title=DBFZ/Android_16/Frame_Data']
 
     def parse(self, response):
-        for row in response.css('.wikitable tr'):
+
+        for row in response.xpath('//*[@class="wikitable"]//tbody//tr'):
             yield {
-                'Version': row.css('th::text').extract(),
-                'Damage': row.css('td:nth-child(1)::text').extract(),
-                'Attribute': row.css('td:nth-child(2)::text').extract(),
-                'Guard': row.css('td:nth-child(3)::text').extract(),
-                'Startup': row.css('td:nth-child(4)::text').extract(),
-                'Active': row.css('td:nth-child(5)::text').extract(),
-                'Recovery': row.css('td:nth-child(6)::text').extract(),
-                'Frame Adv.': row.css('td:nth-child(7)::text').extract(),
-                'Level': row.css('td:nth-child(8)::text').extract(),
-                'Blockstun': row.css('td:nth-child(9)::text').extract(),
-                'Hitstun': row.css('td:nth-child(10)::text').extract(),
-                'Untech': row.css('td:nth-child(11)::text').extract(),
-                'Invul': row.css('td:nth-child(12)::text').extract(),
-                'Hitbox': row.css('td:nth-child(13)::text').extract(),
+                'Version': row.xpath('th//text()').extract_first(),
+                'Damage': row.xpath('td[1]//text()').extract_first(),
+                'Attribute': row.xpath('td[2]//text()').extract_first(),
+                'Guard': row.xpath('td[3]//text()').extract_first(),
+                'Startup': row.xpath('td[4]//text()').extract_first(),
+                'Active': row.xpath('td[5]//text()').extract_first(),
+                'Recovery': row.xpath('td[6]//text()').extract_first(),
+                'Frame Adv.': row.xpath('td[7]//text()').extract_first(),
+                'Level': row.xpath('td[8]//text()').extract_first(),
+                'Blockstun': row.xpath('td[9]//text()').extract_first(),
+                'Hitstun': row.xpath('td[10]//text()').extract_first(),
+                'Untech': row.xpath('td[11]//text()').extract_first(),
+                'Invul': row.xpath('td[12]//text()').extract_first(),
+                'Hitbox': row.xpath('td[13]//text()').extract_first(),
                 'Notes': '',
-
-
-
             }
 
 
